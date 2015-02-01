de061f41b8b3389e08441809fd7e6a1c770c701d
Esteban <laplansk@gmail.com>
2015-02-01 13:46:26 -0800

    complete some more exercises

--- ex10.py
+++ ex10.py
@@ -1,1 +1,25 @@
 __author__ = 'laplansk'
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+# some escaped characters
+
+print "\\ \' \" \a \b \f"
+
+while True:
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r",
--- ex7.py
+++ ex7.py
@@ -1,1 +1,33 @@
 __author__ = 'laplansk'
+
+# This line prints a string
+print "Mary had a little lamb."
+
+#This line strpints a string, using 'snow' in place of the format variable %s
+print "Its fleece was white as %s." % 'snow'
+
+# This line prints a string
+print "And everywhere that Mary went."
+
+# This line prints a . character 10 times
+print "." * 10 # what'd that do?
+
+# These lines assign 1 character strings to several variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# These lines print the concatenation of the end vars
+# The comma makes the result all appear on a single line
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
